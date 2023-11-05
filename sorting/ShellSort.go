1e1c3527866ee6838b3108b404dba284b62756bc
David Walsh <20276885@studentmail.ul.ie>
2023-11-05 16:27:39 +0000

    Adding Shell Sort

--- sorting/ShellSort.go
+++ sorting/ShellSort.go
@@ -1,1 +1,28 @@
 package sorting
+
+import "sync"
+
+// Concurrent shell sort function
+func ShellSort(arr []int) []int {
+	n := len(arr)
+	wg := sync.WaitGroup{}
+
+	// Start with a big gap, then reduce the gap
+	for gap := n / 2; gap > 0; gap /= 2 {
+		// Do a gapped insertion sort for this gap size.
+		for i := gap; i < n; i++ {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				temp := arr[i]
+				j := i
+				for ; j >= gap && arr[j-gap] > temp; j -= gap {
+					arr[j] = arr[j-gap]
+				}
+				arr[j] = temp
+			}(i)
+		}
+		wg.Wait() // Wait for all goroutines to finish
+	}
+	return arr
+}
