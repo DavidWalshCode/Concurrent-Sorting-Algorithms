3b1d74f04595eb12ed3c7a00586f6b8c31320463
David Walsh <20276885@studentmail.ul.ie>
2023-11-05 16:27:12 +0000

    Adding Counting Sort

--- sorting/CountingSort.go
+++ sorting/CountingSort.go
@@ -1,1 +1,44 @@
 package sorting
+
+import "sync"
+
+// Concurrent counting sort algorithm
+func CountingSort(input []int, maxVal int) []int {
+	// Find the range of input values
+	minVal := maxVal
+	for _, num := range input {
+		if num < minVal {
+			minVal = num
+		}
+	}
+
+	// Initialize the slices for counting and the result
+	offset := -minVal
+	size := maxVal - minVal + 1
+	count := make([]int, size)
+	result := make([]int, len(input))
+
+	// Concurrently count the occurrences of each number
+	var wait sync.WaitGroup
+	for _, num := range input {
+		wait.Add(1)
+		go func(n int) {
+			defer wait.Done()
+			count[n+offset]++
+		}(num)
+	}
+	wait.Wait()
+
+	// Sum up the counts
+	for i := 1; i < size; i++ {
+		count[i] += count[i-1]
+	}
+
+	// Build the output array
+	for _, num := range input {
+		result[count[num+offset]-1] = num
+		count[num+offset]--
+	}
+
+	return result
+}
