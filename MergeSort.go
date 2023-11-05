a42fce606c88916b2d4ec282e592f605443cd4fa
David Walsh <20276885@studentmail.ul.ie>
2023-11-05 00:09:13 +0000

    Adding comments to merge sort

--- MergeSort.go
+++ MergeSort.go
@@ -8,47 +8,58 @@ import (
 	"time"
 )
 
-// ReadCSV reads integers from a CSV file.
+// Reads integers from a CSV file
 func ReadCSV(filename string) ([]int, error) {
+	// Open the CSV file for reading
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
 
-	reader := csv.NewReader(file)
+	defer file.Close() // Ensure the file is closed when the function returns
+
+	reader := csv.NewReader(file) // Create a new CSV reader
+
+	// Read all records from the CSV
 	records, err := reader.ReadAll()
 	if err != nil {
 		return nil, err
 	}
 
-	var data []int
+	var data []int // Initialize a slice to hold the integers
+
+	// Loop through each record (row) in the CSV
 	for _, record := range records {
+		// Loop through each field (column) in the record
 		for _, value := range record {
+			// Convert the string field to an integer
 			num, err := strconv.Atoi(value)
 			if err != nil {
 				return nil, err
 			}
-			data = append(data, num)
+			data = append(data, num) // Append the integer to the data slice
 		}
 	}
 
 	return data, nil
 }
 
-// WriteCSV writes integers to a CSV file.
+// Writes integers to a CSV file
 func WriteCSV(filename string, data []int) error {
+	// Create the CSV file for writing
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer file.Close() // Ensure the file is closed when the function returns
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
+	writer := csv.NewWriter(file) // Create a new CSV writer
+	defer writer.Flush()          // Ensure any buffered data is written when the function returns
 
+	// Loop through each integer in the data slice
 	for _, value := range data {
-		record := []string{strconv.Itoa(value)}
+		record := []string{strconv.Itoa(value)} // Convert the integer to a string and create a record
+		// Write the record to the CSV
 		if err := writer.Write(record); err != nil {
 			return err
 		}
@@ -57,35 +68,42 @@ func WriteCSV(filename string, data []int) error {
 	return nil
 }
 
-// mergeSort is a concurrent implementation of the merge sort algorithm.
+// Concurrent implementation of the merge sort algorithm
 func mergeSort(data []int) []int {
+	// If the data slice is empty or has one element, it's already sorted
 	if len(data) <= 1 {
 		return data
 	}
 
-	mid := len(data) / 2
-	var wait sync.WaitGroup
-	wait.Add(2)
+	mid := len(data) / 2    // Find the middle index
+	var wait sync.WaitGroup // Declare a wait group to synchronize goroutines
+	wait.Add(2)             // Add two counts to the wait group for the two goroutines we will launch
 
-	var left, right []int
+	var left, right []int // Declare slices to hold the left and right halves
+
+	// Concurrently sort the left half
 	go func() {
 		left = mergeSort(data[:mid])
 		wait.Done()
 	}()
+	// Concurrently sort the right half
 	go func() {
 		right = mergeSort(data[mid:])
 		wait.Done()
 	}()
-	wait.Wait()
 
-	return merge(left, right)
+	wait.Wait() // Wait for both halves to be sorted
+
+	return merge(left, right) // Merge the sorted halves and return the result
 }
 
-// merge merges two sorted slices.
+// Merges the two sorted slices together
 func merge(left, right []int) []int {
-	result := make([]int, 0, len(left)+len(right))
-	i, j := 0, 0
 
+	result := make([]int, 0, len(left)+len(right)) // Create a slice to hold the merged result
+	i, j := 0, 0                                   // Initialize indices for iterating over the left and right slices
+
+	// Merge the slices until one is exhausted
 	for i < len(left) && j < len(right) {
 		if left[i] <= right[j] {
 			result = append(result, left[i])
@@ -96,6 +114,7 @@ func merge(left, right []int) []int {
 		}
 	}
 
+	// Append any remaining elements from left and right
 	result = append(result, left[i:]...)
 	result = append(result, right[j:]...)
 
@@ -109,20 +128,18 @@ func main() {
 		panic(err)
 	}
 
-	// Start the timer
-	startTime := time.Now()
+	startTime := time.Now() // Start the timer to measure execution time
 
-	// Sort data
-	sortedData := mergeSort(data)
+	sortedData := mergeSort(data) // Sort the data using the concurrent merge sort algorithm
 
-	// Stop the timer and print the execution time
+	// Calculate the elapsed time since the timer started
 	elapsed := time.Since(startTime)
 	println("Execution time:", elapsed.Seconds(), "seconds")
 	println("Execution time:", elapsed.Milliseconds(), "milliseconds")
 	println("Execution time:", elapsed.Microseconds(), "microseconds")
 	println("Execution time:", elapsed.Nanoseconds(), "nanoseconds")
 
-	// Write sorted data to out.csv
+	// Write the sorted data to out.csv
 	if err := WriteCSV("out.csv", sortedData); err != nil {
 		panic(err)
 	}
