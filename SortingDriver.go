a2c79f0fe88c234037fc31dab2b5676fcb0e9c8f
David Walsh <20276885@studentmail.ul.ie>
2023-11-13 23:32:57 +0000

    Updating formatting for execution times and number of sorted data

--- SortingDriver.go
+++ SortingDriver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Concurrent-Sort-Algorithms/readandwrite"
 	"Concurrent-Sort-Algorithms/sorting"
+	"fmt"
 	"time"
 )
 
@@ -15,22 +16,23 @@ func main() {
 
 	startTime := time.Now() // Start the timer to measure execution time
 
-	sortedData := sorting.MergeSort(data) // Sort the data using the concurrent merge sort algorithm
+	//sortedData := sorting.MergeSort(data) // Sort the data using the concurrent merge sort algorithm
 	//sortedData := sorting.MergeSortAlt(data) // Sort the data using the concurrent merge sort algorithm
 	//sortedData := sorting.CountingSort(data, len(data)) // Sort the data using the concurrent counting sort algorithm
-	//sortedData := sorting.CountingSortAlt(data, len(data)) // Sort the data using the concurrent counting sort algorithm
+	sortedData := sorting.CountingSortAlt(data, len(data)) // Sort the data using the concurrent counting sort algorithm
 	//sortedData := sorting.HeapSort(data) // Sort the data using the concurrent heap sort algorithm
 	//sortedData := sorting.ShellSort(data) // Sort the data using the concurrent shell sort algorithm
 
 	// Calculate the elapsed time since the timer started
 	elapsed := time.Since(startTime)
-	println("Execution time:", elapsed.Seconds(), "seconds")
-	println("Execution time:", elapsed.Milliseconds(), "milliseconds")
-	println("Execution time:", elapsed.Microseconds(), "microseconds")
-	println("Execution time:", elapsed.Nanoseconds(), "nanoseconds")
+	fmt.Printf("Sorted %d numbers in:\n", len(data))
+	println(" -", elapsed.Seconds(), "seconds")
+	println(" -", elapsed.Milliseconds(), "milliseconds")
+	println(" -", elapsed.Microseconds(), "microseconds")
+	println(" -", elapsed.Nanoseconds(), "nanoseconds")
 
 	// Write the sorted data to out.csv
-	if err := readwrite.WriteCSV("output/outMergeSort.csv", sortedData); err != nil {
+	if err := readwrite.WriteCSV("output/outCountingSortAlt.csv", sortedData); err != nil {
 		panic(err)
 	}
 }
