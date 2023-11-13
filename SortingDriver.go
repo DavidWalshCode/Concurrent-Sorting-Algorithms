8ded5cb38b43041671f405caec02d5eb1bbcb350
David Walsh <20276885@studentmail.ul.ie>
2023-11-13 23:11:45 +0000

    Update SortingDriver

--- SortingDriver.go
+++ SortingDriver.go
@@ -15,8 +15,8 @@ func main() {
 
 	startTime := time.Now() // Start the timer to measure execution time
 
-	//sortedData := sorting.MergeSort(data) // Sort the data using the concurrent merge sort algorithm
-	sortedData := sorting.MergeSortAlt(data) // Sort the data using the concurrent merge sort algorithm
+	sortedData := sorting.MergeSort(data) // Sort the data using the concurrent merge sort algorithm
+	//sortedData := sorting.MergeSortAlt(data) // Sort the data using the concurrent merge sort algorithm
 	//sortedData := sorting.CountingSort(data, len(data)) // Sort the data using the concurrent counting sort algorithm
 	//sortedData := sorting.CountingSortAlt(data, len(data)) // Sort the data using the concurrent counting sort algorithm
 	//sortedData := sorting.HeapSort(data) // Sort the data using the concurrent heap sort algorithm
@@ -30,7 +30,7 @@ func main() {
 	println("Execution time:", elapsed.Nanoseconds(), "nanoseconds")
 
 	// Write the sorted data to out.csv
-	if err := readwrite.WriteCSV("output/outMergeSortAlt.csv", sortedData); err != nil {
+	if err := readwrite.WriteCSV("output/outMergeSort.csv", sortedData); err != nil {
 		panic(err)
 	}
 }
