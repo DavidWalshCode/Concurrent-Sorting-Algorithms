86e210af1e24cef62a8db926ceadd18dee4420ab
David Walsh <20276885@studentmail.ul.ie>
2023-11-13 23:51:03 +0000

    Making skeleton work with my sorting alg

--- skeleton.go
+++ skeleton.go
@@ -7,7 +7,9 @@ import (
 	"io"
 	"log"
 	"os"
+	"runtime"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -23,18 +25,19 @@ func main() {
 
 	// This is where your sort method on the numbers array goes.
 	// Sort numbers here.
+	sortedNumbers := countingSort(numbers)
 
 	// Calculate the elapsed time.
 	elapsed := time.Since(start)
 
 	// Write the sorted numbers to the output CSV file. Change the studentID to yours.
-	err = writeNumbers("out(20276885).csv", numbers)
+	err = writeNumbers("out(20276885).csv", sortedNumbers)
 	if err != nil {
 		log.Fatalf("Error writing numbers: %v", err)
 	}
 
 	// Print the number of sorted elements and the time taken.
-	fmt.Printf("Sorted %d numbers in %s.\n", len(numbers), elapsed)
+	fmt.Printf("Sorted %d numbers in %s.\n", len(sortedNumbers), elapsed)
 }
 
 // readNumbers reads integers from a CSV file and returns them as a slice.
@@ -94,4 +97,73 @@ func writeNumbers(filename string, numbers []int) error {
 	}
 
 	return nil
+}
+
+func countingSort(data []int) []int {
+	// Find the range of data values
+	maxVal := len(data)
+	minVal := data[0]
+	for _, num := range data {
+		if num < minVal {
+			minVal = num
+		}
+		if num > maxVal {
+			maxVal = num
+		}
+	}
+
+	offset := -minVal
+	size := maxVal - minVal + 1
+	result := make([]int, len(data))
+
+	// Use the number of CPU cores to limit concurrency
+	numCPU := runtime.NumCPU()
+	chunkSize := (len(data) + numCPU - 1) / numCPU
+
+	// Create a slice of slices to hold local counts
+	localCounts := make([][]int, numCPU)
+	for i := range localCounts {
+		localCounts[i] = make([]int, size)
+	}
+
+	var wait sync.WaitGroup
+
+	// Process chunks concurrently
+	for i := 0; i < numCPU; i++ {
+		wait.Add(1)
+		go func(chunkStart int) {
+			defer wait.Done()
+			chunkEnd := chunkStart + chunkSize
+			if chunkEnd > len(data) {
+				chunkEnd = len(data)
+			}
+			localCount := localCounts[chunkStart/chunkSize]
+			for _, num := range data[chunkStart:chunkEnd] {
+				localCount[num+offset]++
+			}
+		}(i * chunkSize)
+	}
+
+	wait.Wait()
+
+	// Merge local counts
+	globalCount := make([]int, size)
+	for _, localCount := range localCounts {
+		for i := 0; i < size; i++ {
+			globalCount[i] += localCount[i]
+		}
+	}
+
+	// Sum up the counts
+	for i := 1; i < size; i++ {
+		globalCount[i] += globalCount[i-1]
+	}
+
+	// Build the output array
+	for _, num := range data {
+		result[globalCount[num+offset]-1] = num
+		globalCount[num+offset]--
+	}
+
+	return result
 }
